fea2e4cb6d5c9eed4b295d113186a1d12050d903
Daniel Budusan <danibudusan@gmail.com>
2024-02-08 14:49:14 +0200

    t2

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -16,6 +16,8 @@ struct ContentView: View {
             Text("Hello, world!")
             Text("Hello, world!")
             Text("Hello, world!")
+            Text("Hello, world!")
+            Text("Hello, world!")
           
         }
         .padding()
