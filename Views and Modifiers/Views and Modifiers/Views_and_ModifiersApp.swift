3714e55319cb539d9f796ed922f172a3077c9473
Daniel Budusan <danibudusan@gmail.com>
2024-02-13 15:55:23 +0200

    Challenge tasks, Views and modifiers

---
+++ Views and Modifiers/Views and Modifiers/Views_and_ModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  Views_and_ModifiersApp.swift
+//  Views and Modifiers
+//
+//  Created by Daniel Budusan on 13.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct Views_and_ModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
