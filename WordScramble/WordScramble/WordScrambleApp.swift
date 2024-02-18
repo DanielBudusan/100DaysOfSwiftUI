8d179c2eb1093d5f74a2b4d6d2c9f5e1b73f5258
Daniel Budusan <danibudusan@gmail.com>
2024-02-18 21:12:51 +0200

    Word Scramble app

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Daniel Budusan on 16.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
